040c0b011bc12b0330346237f3ff6c456798f667
Ido Nov <idonov8@gmail.com>
2021-03-20 16:10:11 +0200

    Add basic search to list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PlayerCard, { Player } from './PlayerCard'
 
 interface Props {
     items: Array<Player>;
 }
 const List: React.FC<Props> = ({items}) => {
-    const listItems = items.map(
-        (item) => <PlayerCard player={item}/>
-        )
+    const [searchField, setSearchField] = useState('')
+    const listItems = items
+                        .filter(item => (item.first_name+" "+item.last_name).toLowerCase().includes(searchField.toLowerCase()))
+                        .map(
+                            (item) => <PlayerCard player={item}/>
+                            )
     return (
         <div>
+            <label htmlFor="search">Search: </label>
+            <input type="text" name="search" value={searchField} onChange={e => setSearchField(e.target.value)}/>
             <ul>
                 {listItems}
             </ul>
